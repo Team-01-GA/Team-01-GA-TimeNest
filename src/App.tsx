e76033b995dda09e4d1afa8f4b73f94d877fc9e6
Alexander Atanasov <al.atanasov111@gmail.com>
2025-05-26 12:17:04 +0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <button className="btn btn-primary">HELLO WORLD</button>
+        </>
+    );
+}
+
+export default App;
